d937c5d938228b4ed73f7f64c25b1ec868766b59
Guilherme Paes <paes.guilherme@hotmail.com>
2024-02-26 08:42:14 -0300

    Chapter 7

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,10 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import localFont from 'next/font/local';
+
+const Chillax = localFont({
+  src: '../ui/fonts/Chillax-Variable.ttf',
+  display: 'swap',
+});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -6,7 +12,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div
+        className={`flex-grow p-6 md:overflow-y-auto md:p-12 ${Chillax.className}`}
+      >
+        {children}
+      </div>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import localFont from 'next/font/local';
 
-const Chillax = localFont({
-  src: './ui/fonts/Chillax-Variable.ttf',
-  display: 'swap',
-});
-
 export default function RootLayout({
   children,
 }: {
